f007aed3b35e7b1b5062d4730cbbce5833965762
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-29 08:59:24 +0200

    Update r5reloaded-update-depot.sh

--- r5reloaded-update-depot.sh
+++ r5reloaded-update-depot.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-# Start
+echo "# Start"
 echo "# R5Reloaded depot updater."
 
 echo "# Download depot"
 	curl https://github.com/Mauler125/r5sdk/releases/latest/download/depot.zip -LO
 
 echo "# Unzip the depot and replace all existing files.."
-    unzip -o depot.zip
+    	unzip -o depot.zip
 
 echo "# Update done! 🐸"
 
