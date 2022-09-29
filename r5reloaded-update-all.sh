da9ce15ff14eeb1bfd5d773b2461449968e200ea
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-29 08:59:13 +0200

    Update r5reloaded-update-all.sh

--- r5reloaded-update-all.sh
+++ r5reloaded-update-all.sh
@@ -1,35 +1,34 @@
-#!/bin/bash
-# Start
-echo "# R5Reloaded depot and scripts updater."
+echo "# Start"
+echo "# R5Reloaded depot and git scripts updater."
 
 echo "# Download depot"
-    curl https://github.com/Mauler125/r5sdk/releases/latest/download/depot.zip -LO
+    	curl https://github.com/Mauler125/r5sdk/releases/latest/download/depot.zip -LO
 
 echo "# Download scripts"
-    curl https://github.com/Mauler125/scripts_r5/archive/refs/heads/S3_N1094.zip -Lo scripts.zip
+    	curl https://github.com/Mauler125/scripts_r5/archive/refs/heads/S3_N1094.zip -Lo scripts.zip
 
 echo "# Unzip the depot and replace all existing files.."
-    unzip -o depot.zip
+    	unzip -o depot.zip
 
 echo "# Unzip the scripts"
-    unzip -o scripts.zip
+    	unzip -o scripts.zip
 
 echo "# Create scripts directory if it doesn't exist"
-    mkdir -p -- ../platform/scripts
+    	mkdir -p -- ../platform/scripts
 
 echo "# Copy the updated scripts to /platform/scripts and replace the old ones."
-    cd scripts_r5-S3_N1094
-    cp -r * ../platform/scripts
+    	cd scripts_r5-S3_N1094
+    	cp -r * ../platform/scripts
 
 echo "# Update done! 🐸"
 
 echo "# Cleanup.."
-    cd ..
+    	cd ..
 echo "# Remove depot.zip"
-    rm depot.zip
+    	rm depot.zip
 echo "# Remove scripts.zip"
-    rm scripts.zip
-echo "# Remove scripts_r5-S3_N1094"
-    rm -dr scripts_r5-S3_N1094
+    	rm scripts.zip
+echo "# Remove scripts_r5-S3_N1094 folder"
+    	rm -dr scripts_r5-S3_N1094
 
 echo "# End"
