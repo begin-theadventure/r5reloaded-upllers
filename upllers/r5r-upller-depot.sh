b29b3e1cccdbb45238c3dfaa244a222ca54c9d0b
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-29 14:50:25 +0200

    Update r5r-upller-depot.sh

--- upllers/r5r-upller-depot.sh
+++ upllers/r5r-upller-depot.sh
@@ -1,16 +1,16 @@
-echo "# Start"
+echo "# Start."
 echo "# R5Reloaded depot updater/installer."
 
-echo "# Download depot"
+echo "# Download the depot."
 	curl https://github.com/Mauler125/r5sdk/releases/latest/download/depot.zip -LO
 
-echo "# Unzip the depot and replace all existing files.."
+echo "# Unzip the depot and replace all existing files."
     	unzip -o depot.zip
 
 echo "# Update done! 🐸"
 
-echo "# Cleanup.."
-echo "# Remove depot.zip"
+echo "# Cleanup."
+echo "# Remove depot.zip."
 	rm depot.zip
 
-echo "# End"
+echo "# End."
