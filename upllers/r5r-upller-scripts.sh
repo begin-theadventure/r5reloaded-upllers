25a39463ef5ea78dbf0f4f3ad6c027e872f73238
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-29 14:50:54 +0200

    Update r5r-upller-scripts.sh

--- upllers/r5r-upller-scripts.sh
+++ upllers/r5r-upller-scripts.sh
@@ -1,26 +1,26 @@
-echo "# Start"
+echo "# Start."
 echo "# R5Reloaded git scripts updater/installer."
 
-echo "# Download the scripts"
+echo "# Download the scripts."
     	curl https://github.com/Mauler125/scripts_r5/archive/refs/heads/S3_N1094.zip -Lo scripts.zip
 
-echo "# Unzip the scripts"
+echo "# Unzip the scripts."
     	unzip -o scripts.zip
 
-echo "# Create scripts directory if it doesn't exist"
+echo "# Create a scripts directory if it doesn't exist."
     	mkdir -p -- ../platform/scripts
 
-echo "# Copy the updated scripts to /platform/scripts and replace the old ones..."
+echo "# Replace the scripts with new ones."
     	cd scripts_r5-S3_N1094
     	cp -r * ../platform/scripts
 
 echo "# Update done! 🐸"
 
-echo "# Cleanup.."
+echo "# Cleanup."
     	cd ..
-echo "# Remove scripts.zip"
+echo "# Remove scripts.zip."
     	rm scripts.zip
-echo "# Remove scripts_r5-S3_N1094 folder"
+echo "# Remove scripts_r5-S3_N1094 folder."
     	rm -dr scripts_r5-S3_N1094
 
-echo "# End"
+echo "# End."
