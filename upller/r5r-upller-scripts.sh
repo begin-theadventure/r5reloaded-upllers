ceaeabaefd409a4106cd71ed221dc1ba1cc47743
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-29 11:02:06 +0200

    Update and rename r5reloaded-update-scripts.sh to upller/r5r-upller-scripts.sh

--- upller/r5r-upller-scripts.sh
+++ upller/r5r-upller-scripts.sh
@@ -1,5 +1,5 @@
 echo "# Start"
-echo "# R5Reloaded git scripts updater."
+echo "# R5Reloaded git scripts updater/installer."
 
 echo "# Download scripts"
     	curl https://github.com/Mauler125/scripts_r5/archive/refs/heads/S3_N1094.zip -Lo scripts.zip
